7db67bd627564d91f7ee6cf2a22237b7155a1af3
deepinder10 <sdeepinder8@gmail.com>
2015-10-17 17:11:53 +0530

    Created Tabs Activity

---
+++ app/src/main/java/shugal/com/mattendance/DaysActivity.java
@@ -0,0 +1,13 @@
+package shugal.com.mattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaysActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_days);
+    }
+}

3b5e8070bc1bfcd92a6f8b9e0f689626b3691ced
Abhishek Raina <araina97@gmail.com>
2015-10-21 03:08:13 +0530

    Added Database Connectivity for adding lectures

--- app/src/main/java/shugal/com/mattendance/MainActivity.java
+++ app/src/main/java/shugal/com/mattendance/MainActivity.java
@@ -1,16 +1,33 @@
 package shugal.com.mattendance;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener {
+
+    ListView lectureList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements
 
         // timeTable = (Button) findViewById(R.id.timetableButton);
 
+
+        // Navigation Drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -31,6 +50,19 @@ public class MainActivity extends AppCompatActivity implements
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        // Floating Action Button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewLecture();
+            }
+        });
+
+        lectureList = (ListView) findViewById(R.id.list_of_lectures);
+
+        printLectures();
     }
 
     @Override
@@ -63,5 +95,79 @@ public class MainActivity extends AppCompatActivity implements
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void addNewLecture() {
+        final Context context = this;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View dialogView = inflater.inflate(R.layout.new_lecture, null);
+
+        final AlertDialog.Builder customEventDialog = new AlertDialog.Builder(context);
+
+        TextInputLayout inputLectureName = (TextInputLayout) dialogView.findViewById(R.id.input_layout_lecture_name);
+        final EditText lecturename = (EditText) dialogView.findViewById(R.id.lecture_name);
+        //amountText.setText("0");
+
+        customEventDialog.setView(dialogView);
+        customEventDialog.setTitle("New Lecture");
+        customEventDialog.setCancelable(true);
+
+        customEventDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (lecturename.getText().toString().equals("")) {
+                    String message = "You did not Enter any Lecture";
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                } else {
+                    String nameOfLecture = lecturename.getText().toString();
+
+                    LectureData data = new LectureData(nameOfLecture, 0, 0);
+                    DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+                    db.addLecture(data);
+                    printLectures();
+                    Toast.makeText(getApplicationContext(), "Lecture Added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        customEventDialog.create().show();
+    }
+
+    private void printLectures() {
+
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        //Gets date from this activity
+
+        View emptyLinearLayout = findViewById(R.id.frame);
+
+
+        if (db.isLectureListEmpty()) {
+            lectureList.setVisibility(View.GONE);
+            emptyLinearLayout.setVisibility(View.VISIBLE);
+            TextView emptyText = (TextView) findViewById(R.id.emptyListText);
+            Typeface
+                    tf= Typeface.createFromAsset(getAssets(), "fonts/empty_list.ttf");
+            emptyText.setTypeface(tf);
+
+        } else {
+            emptyLinearLayout.setVisibility(View.GONE);
+            lectureList.setVisibility(View.VISIBLE);
+            List<LectureData> contacts = db.showAllLectures();
+
+            LectureCustomList adapter = new LectureCustomList(this, contacts);
+            lectureList.setAdapter(adapter);
+        }
+
+        db.close();
     }
 }

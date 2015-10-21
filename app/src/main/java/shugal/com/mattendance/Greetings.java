9dd6897669969105fd4d208ca5040b30a028c196
Abhishek Raina <araina97@gmail.com>
2015-10-21 19:41:41 +0530

    Full Working intro added

--- app/src/main/java/shugal/com/mattendance/Greetings.java
+++ app/src/main/java/shugal/com/mattendance/Greetings.java
@@ -1,11 +1,10 @@
 package shugal.com.mattendance;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntro2;
-import com.github.paolorotolo.appintro.AppIntroFragment;
 
 /**
  * Created by abhishek on 20/10/15.
@@ -13,16 +12,12 @@ import com.github.paolorotolo.appintro.AppIntroFragment;
 public class Greetings extends AppIntro2 {
     @Override
     public void init(Bundle savedInstanceState) {
-        addSlide(AppIntroFragment.newInstance("Attendance Tracker",
-                "This App will help you track your attendance", R.drawable.playstore_icon, Color.LTGRAY));
 
-        addSlide(AppIntroFragment.newInstance("Attendance Tracker",
-                "This App will help you track your attendance", R.drawable.playstore_icon, Color.LTGRAY));
+        addSlide(SampleSlide.newInstance(R.layout.intro1));
 
-        addSlide(AppIntroFragment.newInstance("Attendance Tracker",
-                "This App will help you track your attendance", R.drawable.playstore_icon, Color.LTGRAY));
+        addSlide(SampleSlide.newInstance(R.layout.intro2));
 
-
+        addSlide(SampleSlide.newInstance(R.layout.intro3));
         showDoneButton(true);
 
     }

6f43959151ad10b15b6df8e4a4a2a201310fe87c
Abhishek Raina <araina97@gmail.com>
2015-10-24 17:59:56 +0530

    Almost completed app

--- app/src/main/java/shugal/com/mattendance/TodayAttendance.java
+++ app/src/main/java/shugal/com/mattendance/TodayAttendance.java
@@ -1,29 +1,28 @@
 package shugal.com.mattendance;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Locale;
+import java.util.List;
 
 public class TodayAttendance extends AppCompatActivity {
 
-    ListView listView;
-    TodayCustomList adapter = null;
+    ListView lectureList;
+    TextView mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,110 +31,95 @@ public class TodayAttendance extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Mark Todays's attendance");
 
-        DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+        mark = (TextView) findViewById(R.id.mark);
+        Typeface tf= Typeface.createFromAsset(getAssets(), "fonts/empty_list.ttf");
+        mark.setTypeface(tf);
 
-        listView = (ListView) findViewById(R.id.todayAttendance);
+        lectureList = (ListView) findViewById(R.id.list_of_lectures);
+        printLectures();
 
-        String weekDay;
-        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
-
-        Calendar calendar = Calendar.getInstance();
-        weekDay = dayFormat.format(calendar.getTime());
-
-        ArrayList<TimetableData> contacts = db.showTimetable(weekDay);
-        //db.showAllTimetable(DAY);
-
-        adapter = new TodayCustomList(getApplicationContext(), contacts);
-        listView.setAdapter(adapter);
-
-        checkButtonClick();
-    }
-
-    private void checkButtonClick() {
-        Button myButton = (Button) findViewById(R.id.submitId);
-        myButton.setOnClickListener(new View.OnClickListener() {
-
+        lectureList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-
-
-                StringBuffer responseText = new StringBuffer();
-                responseText.append("The following were selected...\n");
-
-                ArrayList<TimetableData> subjectsList = adapter.subjectsList;
-                for (int i = 0; i < subjectsList.size(); i++) {
-                    TimetableData subj = subjectsList.get(i);
-                    if (subj.equals(subj.getLecture_name())) {
-                        responseText.append("\n" + subj.getLecture_name());
-                    }
-                }
-
-                Toast.makeText(getApplicationContext(),
-                        responseText, Toast.LENGTH_LONG).show();
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LectureData data = (LectureData) parent.getItemAtPosition(position);
+                startEditDialog(data);
             }
         });
+
     }
 
-    public class TodayCustomList extends ArrayAdapter<TimetableData> {
-
-        private ArrayList<TimetableData> subjectsList;
-
-        public TodayCustomList(Context context,
-                               ArrayList<TimetableData> countryList) {
-            super(context, 0, countryList);
-            this.subjectsList = new ArrayList<TimetableData>();
-            this.subjectsList.addAll(subjectsList);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            TimetableData data = getItem(position);
-
-            ViewHolder holder = null;
-            Log.v("ConvertView", String.valueOf(position));
+    private void startEditDialog(LectureData data) {
+        final Context context = this;
+        LayoutInflater inflater = LayoutInflater.from(context);
 
 
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_list_today_attendance, null);
+        View dialogView = inflater.inflate(R.layout.attendance_dialog, null);
 
-                holder = new ViewHolder();
-                holder.name = (CheckBox) convertView.findViewById(R.id.checkboxTick);
-                convertView.setTag(holder);
+        final AlertDialog.Builder customEventDialog = new AlertDialog.Builder(context);
 
-                holder.name.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        CheckBox cb = (CheckBox) v;
-                        TimetableData subdata = (TimetableData) cb.getTag();
-                        Toast.makeText(getContext(),
-                                "Clicked on Checkbox: " + cb.getText() +
-                                        " is " + cb.isChecked(),
-                                Toast.LENGTH_LONG).show();
+        final RadioGroup radioGroup = (RadioGroup) dialogView.findViewById(R.id.radioGroup);
+        final RadioButton presentBtn = (RadioButton) dialogView.findViewById(R.id.presentBtn);
+        final RadioButton absentBtn = (RadioButton) dialogView.findViewById(R.id.absentBtn);
+        RadioButton noClass = (RadioButton) dialogView.findViewById(R.id.noClass);
 
-                    }
-                });
+        customEventDialog.setView(dialogView);
+        customEventDialog.setTitle("Mark Attendance");
+        customEventDialog.setCancelable(true);
+        customEventDialog.setPositiveButton("Mark", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
-            } else {
-                holder = (ViewHolder) convertView.getTag();
+                if (presentBtn.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "PRESENT", Toast.LENGTH_SHORT).show();
+                } else if (absentBtn.isChecked()) {
+                    Toast.makeText(getApplicationContext(), "Absent", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "No Class Marked", Toast.LENGTH_SHORT).show();
+                }
             }
-
-            TextView lectureName = (TextView) convertView.findViewById(R.id.lectureName);
-
-            lectureName.setText(data.getLecture_name());
-            TimetableData country = subjectsList.get(position);
-            holder.name.setTag(country);
-
-            return convertView;
-
-        }
-
-        private class ViewHolder {
-            CheckBox name;
-        }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
 
 
+        customEventDialog.create().show();
     }
+
+    private void printLectures() {
+
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+        //Gets date from this activity
+
+        View emptyLinearLayout = findViewById(R.id.frame);
+
+
+        if (db.isLectureListEmpty()) {
+            lectureList.setVisibility(View.GONE);
+            emptyLinearLayout.setVisibility(View.VISIBLE);
+            TextView emptyText = (TextView) findViewById(R.id.emptyListText);
+            Typeface tf= Typeface.createFromAsset(getAssets(), "fonts/empty_list.ttf");
+            emptyText.setTypeface(tf);
+
+        } else {
+            emptyLinearLayout.setVisibility(View.GONE);
+            lectureList.setVisibility(View.VISIBLE);
+            List<LectureData> contacts = db.showAllLectures();
+
+
+            TodayAttendanceCustomListAdapter adapter =
+                    new TodayAttendanceCustomListAdapter(getApplicationContext(), contacts);
+            lectureList.setAdapter(adapter);
+        }
+
+        db.close();
+    }
+
 
 }

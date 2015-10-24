d45b638175213db5f8667a7de314034b091cddfb
deepinder10 <sdeepinder8@gmail.com>
2015-10-25 03:14:47 +0530

    Info Added

--- app/src/main/java/shugal/com/mattendance/MainActivity.java
+++ app/src/main/java/shugal/com/mattendance/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -253,6 +254,28 @@ public class MainActivity extends AppCompatActivity implements
         db.close();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.info) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 

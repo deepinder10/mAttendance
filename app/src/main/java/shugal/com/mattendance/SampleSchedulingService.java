31f3d7a1ad38d7a7d2f49dea43604517ce87a7a4
deepinder10 <sdeepinder8@gmail.com>
2015-10-26 11:42:11 +0530

    Notifications 5 min working

--- app/src/main/java/shugal/com/mattendance/SampleSchedulingService.java
+++ app/src/main/java/shugal/com/mattendance/SampleSchedulingService.java
@@ -7,16 +7,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Deepinder on 10/24/2015.
  */
 public class SampleSchedulingService extends IntentService {
     public static final int NOTIFICATION_ID = 1;
+    NotificationCompat.Builder m5min;
     private NotificationManager mNotificationManager;
-
+    private int l, k;
     public SampleSchedulingService() {
         super("SchedulingService");
     }
@@ -31,6 +36,15 @@ public class SampleSchedulingService extends IntentService {
         mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        DatabaseHelper db = new DatabaseHelper(getApplicationContext());
+
+        String weekDay;
+        SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE", Locale.US);
+
+        Calendar calendar = Calendar.getInstance();
+        weekDay = dayFormat.format(calendar.getTime());
+
+
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, TodayAttendance.class), 0);
 
@@ -42,18 +56,91 @@ public class SampleSchedulingService extends IntentService {
                                 .bigText(msg))
                         .setContentText(msg);
 
+     /*
+            for(int i =0; i<contacts.size();i++){
+                TimetableData data = contacts.get(i);
+                String time = data.getStarting_time();
+                String name = data.getLecture_name();
+                String stHour = time.substring(0, 2);
+                String stMin = time.substring(3, 5);
+                int sHour = Integer.parseInt(stHour);
+                int sMin = Integer.parseInt(stMin);
+                if (sMin >= 0 && sMin < 6 ){
+                    l =sHour - 1;
+                    k= sMin - 5;
+                }
+                else {
+                    l =sHour;
+                    k = sMin - 5;
+                }
+
+                Log.d("5min","5mim + " + l + "  " + k);
+                m5min =
+                        new NotificationCompat.Builder(this)
+                                .setSmallIcon(R.drawable.playstore_icon)
+                                .setContentTitle("mAttendance")
+                                .setStyle(new NotificationCompat.BigTextStyle()
+                                        .bigText(msg))
+                                .setContentText(name + " Class in 5 minutes");
+                m5min.setAutoCancel(true);
+                m5min.setContentIntent(contentIntent);
+            }
+*/
+
         Calendar c = Calendar.getInstance();
         int d = c.get(Calendar.HOUR_OF_DAY);
-
+        int e = c.get(Calendar.MINUTE);
+        int f = c.get(Calendar.SECOND);
 
         mBuilder.setContentIntent(contentIntent);
         mBuilder.setAutoCancel(true);
-        if (d == 21) {
+
+
+        if (d == 21 && e == 1 && f == 10) {
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
         Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
         // Vibrate for 500 milliseconds
         v.vibrate(1000);
         }
 
+        if (!db.isTimetableEmpty(weekDay)) {
+            ArrayList<TimetableData> contacts = db.showTimetable(weekDay);
+
+            for (int i = 0; i < contacts.size(); i++) {
+                TimetableData data = contacts.get(i);
+                String time = data.getStarting_time();
+                String name = data.getLecture_name();
+                String stHour = time.substring(0, 2);
+                String stMin = time.substring(3, 5);
+                int sHour = Integer.parseInt(stHour);
+                int sMin = Integer.parseInt(stMin);
+                if (sMin >= 0 && sMin < 6) {
+                    l = sHour - 1;
+                    k = sMin - 5;
+                } else {
+                    l = sHour;
+                    k = sMin - 5;
+                }
+                if (d == l && e == k && f < 4) {
+                    m5min = new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.playstore_icon)
+                            .setContentTitle("mAttendance")
+                            .setStyle(new NotificationCompat.BigTextStyle()
+                                    .bigText(msg))
+                            .setContentText(name + " Class in 5 minutes");
+
+                    m5min.setAutoCancel(true);
+                    m5min.setContentIntent(contentIntent);
+
+                    mNotificationManager.notify(2, m5min.build());
+                    Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+                    // Vibrate for 500 milliseconds
+                    v.vibrate(1000);
+                }
+            }
+        }
+
+
+        Log.d("exaxt time", "exact time " + d + " " + e);
     }
 }

0847254d3d707b6ab2fa0b79c2699e161b8c7476
Abhishek Raina <araina97@gmail.com>
2015-10-27 01:56:57 +0530

    Added toolbar on email activity

---
+++ app/src/main/java/shugal/com/mattendance/DatewiseData.java
@@ -0,0 +1,10 @@
+package shugal.com.mattendance;
+
+/**
+ * Created by abhishek on 27/10/15.
+ */
+public class DatewiseData {
+    private int id;
+    private String date;
+    private String status;
+}
---
+++ app/src/main/java/shugal/com/mattendance/DatewiseList.java
@@ -0,0 +1,15 @@
+package shugal.com.mattendance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abhishek on 27/10/15.
+ */
+public class DatewiseList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datewisedata_toolbar);
+    }
+}
